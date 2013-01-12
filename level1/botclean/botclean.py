9ef98bd78b2fc07291967114f154f0f170a69d5c
Nick Johnson <nickj21@gmail.com>
2013-01-12 14:35:44 -0800

    updated botclean

--- level1/botclean/botclean.py
+++ level1/botclean/botclean.py
@@ -8,51 +8,76 @@ def next_move(posx, posy, board):
 
 
 def decide_move(posx, posy, board):
+    """
+    If current cell is dirty, clean it
+    Else use BFS to find closest dirty cell to move toward
+
+    queue is used to store neighboring cells
+    checked_grid is used to make sure each cell is added to the queue only once
+    """
+
     # Check if on dirty cell
     if board[posx][posy] == "d":
         return "CLEAN"
 
     current_coordinates = (posx, posy)
-    return determine_next_move(current_coordinates, calculate_target_cell(collections.deque().append(current_coordinates)))
+    queue =  collections.deque().append(current_coordinates)
+    checked_grid = [["-" for i in range(5)] for j in range(5)]
+    checked_grid[posx][posy] = "c"
+    return determine_next_move(current_coordinates, calculate_target_cell(queue, checked_grid))
 
-def calculate_target_cell(queue):
+def calculate_target_cell(queue, grid):
+    if len(queue) == 0:
+        return "DONE"
+
     current_coordinates = queue.popleft()
     if board[current_coordinates[0]][current_coordinates[1]] == "d":
         return current_coordinates
     # Enqueue neighboring cells
     else:
-        for next_coordinates in find_existing_neighbors(current_coordinates):
-            queue.append(next_coordinates)
+        queue.extend(find_existing_neighbors(current_coordinates, grid))
 
-    return calculate_target_cell(queue)
+    return calculate_target_cell(queue, grid)
 
-# Finds neighbors assuming 5x5 board
-def find_existing_neighbors(coordinates):
+
+def find_existing_neighbors(coordinates, grid):
+    """
+    Add neighboring cells of coordinates if applicable and mark as checked on the grid
+    """
+
     neighbors = []
     newcoordinates = (coordinates[0], coordinates[1]+1)
-    if check_coordinates(newcoordinates):
+    if check_coordinates(newcoordinates, grid):
         neighbors.append(newcoordinates)
+        grid[newcoordinates[0]][newcoordinates[1]] = "c"
     newcoordinates = (coordinates[0], coordinates[1]-1)
-    if check_coordinates(newcoordinates):
+    if check_coordinates(newcoordinates, grid):
         neighbors.append(newcoordinates)
+        grid[newcoordinates[0]][newcoordinates[1]] = "c"
     newcoordinates = (coordinates[0]+1, coordinates[1])
-    if check_coordinates(newcoordinates):
+    if check_coordinates(newcoordinates, grid):
         neighbors.append(newcoordinates)
+        grid[newcoordinates[0]][newcoordinates[1]] = "c"
     newcoordinates = (coordinates[0]-1, coordinates[1])
-    if check_coordinates(newcoordinates):
+    if check_coordinates(newcoordinates, grid):
         neighbors.append(newcoordinates)
+        grid[newcoordinates[0]][newcoordinates[1]] = "c"
 
     return neighbors
 
-def check_coordinates(coordinates):
-    return 0 <= coordinates[0] <= 4 and 0 <= coordinates[1] <= 4
+def check_coordinates(coordinates, grid):
+    """
+    Check if coordinates are on 5x5 board and that they are not already in the queue
+    """
+
+    return 0 <= coordinates[0] <= 4 and 4 >= coordinates[1] >= 0 and grid[coordinates[0]][coordinates[1]] == "-"
 
 def determine_next_move(current_coordinates, destination_coordinates):
-    if destination_coordinates[0] - current_coordinates[0] > 0:
+    if destination_coordinates[1] - current_coordinates[1] > 0:
         return "RIGHT"
-    elif destination_coordinates[0] - current_coordinates[0] < 0:
+    elif destination_coordinates[1] - current_coordinates[1] < 0:
         return "LEFT"
-    elif destination_coordinates[1] - current_coordinates[1] > 0:
+    elif destination_coordinates[0] - current_coordinates[0] > 0:
         return "DOWN"
     elif destination_coordinates[0] - current_coordinates[0] < 0:
         return "UP"
--- level1/botclean/botcleanunittest.py
+++ level1/botclean/botcleanunittest.py
@@ -4,48 +4,66 @@ import botclean
 
 __author__ = 'njohnson'
 
-CELLS = {0:"-", 1:"d", 2:"b"}
+CELLS = {0:"-", 1:"d", 2:"b", 3:"c"}
 
 class BotCleanTests(unittest.TestCase):
 
     def setUp(self):
         # list comprehension
-        # initialize 5x5 board to all -
+        # initialize 5x5 board and check grid
         self.board = [["-" for i in range(5)] for j in range(5)]
+        self.grid = [["-" for i in range(5)] for j in range(5)]
 
-    def set_board(self, row0, row1, row2, row3, row4):
-        self.board[0] = [CELLS[row0[0]],CELLS[row0[1]],CELLS[row0[2]],CELLS[row0[3]],CELLS[row0[4]]]
-        self.board[1] = [CELLS[row1[0]],CELLS[row1[1]],CELLS[row1[2]],CELLS[row1[3]],CELLS[row1[4]]]
-        self.board[2] = [CELLS[row2[0]],CELLS[row2[1]],CELLS[row2[2]],CELLS[row2[3]],CELLS[row2[4]]]
-        self.board[3] = [CELLS[row3[0]],CELLS[row3[1]],CELLS[row3[2]],CELLS[row3[3]],CELLS[row3[4]]]
-        self.board[4] = [CELLS[row4[0]],CELLS[row4[1]],CELLS[row4[2]],CELLS[row4[3]],CELLS[row4[4]]]
+    def set_board_or_grid(self, board, row0, row1, row2, row3, row4):
+        board[0] = [CELLS[row0[0]],CELLS[row1[0]],CELLS[row2[0]],CELLS[row3[0]],CELLS[row4[0]]]
+        board[1] = [CELLS[row0[1]],CELLS[row1[1]],CELLS[row2[1]],CELLS[row3[1]],CELLS[row4[1]]]
+        board[2] = [CELLS[row0[2]],CELLS[row1[2]],CELLS[row2[2]],CELLS[row3[2]],CELLS[row4[2]]]
+        board[3] = [CELLS[row0[3]],CELLS[row1[3]],CELLS[row2[3]],CELLS[row3[3]],CELLS[row4[3]]]
+        board[4] = [CELLS[row0[4]],CELLS[row1[4]],CELLS[row2[4]],CELLS[row3[4]],CELLS[row4[4]]]
 
     # If on dirty cell, clean
     def test_clean(self):
         posx = 0
         posy = 0
-        self.set_board( [1,0,0,0,0],
-                        [0,0,0,0,0],
-                        [0,0,0,0,0],
-                        [0,0,0,0,0],
-                        [0,0,0,0,0])
+        self.set_board_or_grid( self.board,
+                                [1,0,0,0,0],
+                                [0,0,0,0,0],
+                                [0,0,0,0,0],
+                                [0,0,0,0,0],
+                                [0,0,0,0,0])
         self.assertEquals("CLEAN", botclean.decide_move(posx, posy, self.board))
 
     def test_check_coordinates(self):
         outside_board = [(-1,0),(6,2),(3,-1),(4,6)]
         inside_board = [(0,0),(4,4),(2,3)]
+        checked_coordinates = (1,4)
+        not_checked_coordinates = (2,4)
+
+        self.set_board_or_grid( self.grid,
+                                [0,0,0,0,0],
+                                [0,0,0,0,0],
+                                [0,0,0,0,0],
+                                [0,0,0,0,0],
+                                [0,3,0,0,0])
 
         for coordinate in outside_board:
-            self.assertFalse(botclean.check_coordinates(coordinate))
+            self.assertFalse(botclean.check_coordinates(coordinate, self.grid))
 
         for coordinate in inside_board:
-            self.assertTrue(botclean.check_coordinates(coordinate))
+            self.assertTrue(botclean.check_coordinates(coordinate, self.grid))
+
+        self.assertFalse(botclean.check_coordinates(checked_coordinates, self.grid))
+        self.assertTrue(botclean.check_coordinates(not_checked_coordinates, self.grid))
 
     def test_determine_next_move(self):
-        self.assertEquals("RIGHT", botclean.determine_next_move((0,0),(1,0)))
-        self.assertEquals("LEFT", botclean.determine_next_move((1,0),(0,0)))
-        self.assertEquals("DOWN", botclean.determine_next_move((0,0),(0,1)))
-        self.assertEquals("UP", botclean.determine_next_move((0,1),(0,0)))
+        """
+        I think when python inputs grid, coordinates will be y,x...
+        """
+
+        self.assertEquals("RIGHT", botclean.determine_next_move((0,0),(0,1)))
+        self.assertEquals("LEFT", botclean.determine_next_move((0,1),(0,0)))
+        self.assertEquals("DOWN", botclean.determine_next_move((0,0),(1,0)))
+        self.assertEquals("UP", botclean.determine_next_move((1,0),(0,0)))
 
 def main():
     unittest.main()
